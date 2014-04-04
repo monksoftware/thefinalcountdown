9c971ad1235019b82d85d07f98aa551c03e29db5
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2014-04-04 17:50:15 +0200

    Create the countdown directive

--- public/app.js
+++ public/app.js
@@ -1,15 +1,23 @@
 angular.module('app', [])
 
-.controller('AppCtrl', function($scope, $timeout) {
-  $scope.message = "Hello";
-  var date = new Date(2014, 3, 11, 18);
-  $scope.countdown = countdown(date);
+.directive('countdown', function($timeout) {
+  return {
+    restrict: "A",
+    template: "{{ countdown.toString() }}",
+    link: function(scope, element, attributes) {
+      var date = attributes.date.split('-');
+      var time = attributes.time.split(':');
+      date = new Date(date[0], date[1] - 1, date[2], time[0], time[1]);
 
-  (function tick() {
-    $scope.countdown = countdown(date);
-    $timeout(tick, 1000);
-  })();
-});
+      (function tick() {
+        scope.countdown = countdown(date);
+        $timeout(tick, 1000);
+      })();
+    }
+  }
+})
+
+.controller('AppCtrl', function($scope, $timeout) {});
 
 $(function(){
   $('h2').fitText();
